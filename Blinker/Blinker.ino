cfda7d7803e0daddb1151c002292c274d3503ef1
Audrey Marullaz <audrey.marullaz@epfl.ch>
2016-03-23 15:03:47 +0100

    Add left, righ and back LEDs

--- Blinker/Blinker.ino
+++ Blinker/Blinker.ino
@@ -4,12 +4,18 @@
  * Description: Activates the blinker with the button
  *
  ***************************************************************************************/
-#include <prismino.h>
-#define BUTTONR 7 // 0,1,2,3,7 for interrupts on Leonardo
-#define BUTTONC 3
-volatile int blinker = 0;
+#define LEDF 10
+#define LEDB 16
+#define LEDR 8
+#define LEDL 9
+#define BUTTONR 7
+#define BUTTONL 5
+#define BUTTONC 6
 
-void rightButton_CB()
+volatile int blinker = 1;
+int del = 40000; // 15625 = 1 sec.
+
+/*void rightButton_CB()
 {
   blinker = 1;
 }
@@ -18,26 +24,62 @@ void centerButton_CB()
 {
   blinker = 0;
 }
-
+*/
 void setup()
 {
-  pinMode(LED, OUTPUT);
-  pinMode(BUTTONR, INPUT);
-  pinMode(BUTTONC, INPUT);
-  digitalWrite(LED, LOW);
-  attachInterrupt(digitalPinToInterrupt(BUTTONR), rightButton_CB, FALLING);
-  attachInterrupt(digitalPinToInterrupt(BUTTONC), centerButton_CB, FALLING);
+  pinMode(LEDF, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+  pinMode(LEDL, OUTPUT);
+  pinMode(LEDR, OUTPUT);
+  pinMode(BUTTONR,INPUT);
+  pinMode(BUTTONL,INPUT);
+  pinMode(BUTTONC,INPUT);
+  
+  digitalWrite(LEDF, LOW);
+  digitalWrite(LEDB, LOW);
+  digitalWrite(LEDL, LOW);
+  digitalWrite(LEDR, LOW);
+
+  cli(); // disable interrupts
+//  PRRO &= ~(1 << PRTIM1); // enable timer1, enabled by default
+  TIMSK1 |= (1 << OCIE1A); //enable output compare A
+  TCCR1A &= ~((1 << COM1A1) | (1 << COM1A0) | (1 << WGM11) | (1 << WGM10)); // remove pwm pins
+  TCCR1B |= ((1 << CS11) | (1 << CS10)); // prescaler to 64, 15625 Hz counter
+  TCCR1B &= ~((1 << CS12) | (1 << WGM13) | (1 << WGM12)); // prescaler & remove PWM pins
+  TCNT1 = 0; // set counter to 0
+  OCR1A = del; // 0x3d09 is 15625 in hex
+  sei(); // enable interrupts
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  char _sreg = SREG; // save sreg
+  OCR1A = TCNT1 + del; // next interrupt in 1s
+
+  if(blinker == 1)
+  {
+    digitalWrite(LEDL, !digitalRead(LEDL));
+    digitalWrite(LEDR, LOW);
+  }
+  else if (blinker == 2)
+  {
+    digitalWrite(LEDR, !digitalRead(LEDR));
+    digitalWrite(LEDL, LOW);
+  }
+  else
+  {
+    digitalWrite(LEDR, LOW);
+    digitalWrite(LEDL, LOW); 
+  }
+  SREG = _sreg; // restore sreg
 }
 
 void loop()
 {
-  if(blinker)
-  {// turn LED on
-  digitalWrite(LED, HIGH);
-  // wait 500 milliseconds
-  delay(500);
-  // turn LED off
-  digitalWrite(LED, LOW);
-  delay(500);
-  }
+  if(!digitalRead(BUTTONR))
+    blinker = 2;
+  if(!digitalRead(BUTTONL))
+    blinker = 1;
+  if(!digitalRead(BUTTONC))
+    blinker = 0;
 }
