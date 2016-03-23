819f96a49c3b7f350e658ccb5fbb4c0f5067237f
Audrey Marullaz <audrey.marullaz@epfl.ch>
2016-03-23 15:06:16 +0100

    Update comments

--- Blinker/Blinker.ino
+++ Blinker/Blinker.ino
@@ -1,7 +1,7 @@
 /***************************************************************************************
  *
  * Title:       Blinker
- * Description: Activates the blinker with the button
+ * Description: De/Activates the blinker with the buttons
  *
  ***************************************************************************************/
 #define LEDF 10
@@ -15,16 +15,6 @@
 volatile int blinker = 1;
 int del = 40000; // 15625 = 1 sec.
 
-/*void rightButton_CB()
-{
-  blinker = 1;
-}
-
-void centerButton_CB()
-{
-  blinker = 0;
-}
-*/
 void setup()
 {
   pinMode(LEDF, OUTPUT);
@@ -44,7 +34,7 @@ void setup()
 //  PRRO &= ~(1 << PRTIM1); // enable timer1, enabled by default
   TIMSK1 |= (1 << OCIE1A); //enable output compare A
   TCCR1A &= ~((1 << COM1A1) | (1 << COM1A0) | (1 << WGM11) | (1 << WGM10)); // remove pwm pins
-  TCCR1B |= ((1 << CS11) | (1 << CS10)); // prescaler to 64, 15625 Hz counter
+  TCCR1B |= ((1 << CS11) | (1 << CS10)); // prescaler to 64, (Replace CS11 by CS12 in this line, and CS12 by CS11 in next line for prescaler = 1024)
   TCCR1B &= ~((1 << CS12) | (1 << WGM13) | (1 << WGM12)); // prescaler & remove PWM pins
   TCNT1 = 0; // set counter to 0
   OCR1A = del; // 0x3d09 is 15625 in hex
