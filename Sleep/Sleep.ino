de7aa850e79e8079e22c92d39856614b02f9c54c
Audrey Marullaz <audrey.marullaz@epfl.ch>
2016-04-20 16:17:54 +0200

    Mise en forme

--- Sleep/Sleep.ino
+++ Sleep/Sleep.ino
@@ -1,35 +1,7 @@
 #include <avr/sleep.h>
  
-/* Sleep Demo Serial
- * -----------------
- * Example code to demonstrate the sleep functions in an Arduino.
- *
- * use a resistor between RX and pin2. By default RX is pulled up to 5V
- * therefore, we can use a sequence of Serial data forcing RX to 0, what
- * will make pin2 go LOW activating INT0 external interrupt, bringing
- * the MCU back to life
- *
- * there is also a time counter that will put the MCU to sleep after 10 secs
- *
- * NOTE: when coming back from POWER-DOWN mode, it takes a bit
- *       until the system is functional at 100%!! (typically <1sec)
- *
- * Copyright (C) 2006 MacSimski 2006-12-30
- * Copyright (C) 2007 D. Cuartielles 2007-07-08 - Mexico DF
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+/* Pin2 go LOW activating INT0 external interrupt, bringing the MCU back to life
+ * Also a time counter that will put the MCU to sleep after 10 secs
  */
  
 int wakePin = 2;                 // pin used for waking up
@@ -43,14 +15,10 @@ int count = 0;                   // counter
 
 void wakeUpNow()        // here the interrupt is handled after wakeup
 {
-  detachInterrupt(0);      // disables interrupt 0 on pin 2 so the
-                             // wakeUpNow code will not be executed
-                             // during normal running time.
+  detachInterrupt(0);   // disable int0 on pin2 = wakeUpNow code wont be executed during normal running time.
   digitalWrite(LEDF, !digitalRead(LEDF));
   // execute code here after wake-up before returning to the loop() function
-  // timers and code using timers (serial.print and more...) will not work here.
-  // we don't really need to execute any special functions here, since we
-  // just want the thing to wake up
+  // /!\ timers and code using timers (serial.print and more...) will not work here.
 }
  
 void setup()
@@ -68,86 +36,35 @@ void setup()
  
   Serial.begin(9600);
  
-  /* Now it is time to enable an interrupt. In the function call
-   * attachInterrupt(A, B, C)
-   * A   can be either 0 or 1 for interrupts on pin 2 or 3.  
-   *
-   * B   Name of a function you want to execute while in interrupt A.
-   *
-   * C   Trigger mode of the interrupt pin. can be:
-   *             LOW        a low level trigger
-   *             CHANGE     a change in level trigger
-   *             RISING     a rising edge of a level trigger
-   *             FALLING    a falling edge of a level trigger
-   *
-   * In all but the IDLE sleep modes only LOW can be used.
-   */
- 
-   //attachInterrupt(0, wakeUpNow, LOW); // use interrupt 0 (pin 2) and run function
-     delay(8000);                                 // wakeUpNow when pin 2 gets LOW
-   Serial.println("Initialisation complete.");
+  delay(8000);
+  Serial.println("Initialisation complete.");
 }
  
 void sleepNow()         // here we put the arduino to sleep
 {
   Serial.println("Entering sleep function");
-
-    attachInterrupt(0,wakeUpNow, LOW); // use interrupt 0 (pin 2) and run function
-                                       // wakeUpNow when pin 2 gets LOW
-    delay(100);
-    
-    /* Now is the time to set the sleep mode. In the Atmega8 datasheet
-     * http://www.atmel.com/dyn/resources/prod_documents/doc2486.pdf on page 35
-     * there is a list of sleep modes which explains which clocks and
-     * wake up sources are available in which sleep mode.
-     *
-     * In the avr/sleep.h file, the call names of these sleep modes are to be found:
-     *
-     * The 5 different modes are:
-     *     SLEEP_MODE_IDLE         -the least power savings
-     *     SLEEP_MODE_ADC
-     *     SLEEP_MODE_PWR_SAVE
-     *     SLEEP_MODE_STANDBY
-     *     SLEEP_MODE_PWR_DOWN     -the most power savings
-     *
-     * For now, we want as much power savings as possible, so we
-     * choose the according
-     * sleep mode: SLEEP_MODE_PWR_DOWN
-     *
-     */  
-     Serial.println("Interrupt attached");
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
- Serial.println("sleep mode set");
-    sleep_enable();          // enables the sleep bit in the mcucr register
-             Serial.println("sleep enabled");                // so sleep is possible. just a safety pin
+  /* attachInterrupt(A, B, C)
+  * A   0 or 1 for int on pin 2 or 3.  
+  * B   Name of a int function
+  * C   Trigger mode (LOW, CHANGE, RISING, FALLING)   *
+  * In all but the IDLE sleep modes only LOW can be used.
+  */
+  attachInterrupt(0, wakeUpNow, LOW);
+  
+  delay(100);
+  
+  Serial.println("Interrupt attached");
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
+  Serial.println("sleep mode set");
+  sleep_enable();          // enables the sleep bit in the mcucr register so sleep is possible. just a safety pin
+  Serial.println("sleep enabled");
  
-    /* Now it is time to enable an interrupt. We do it here so an
-     * accidentally pushed interrupt button doesn't interrupt
-     * our running program. if you want to be able to run
-     * interrupt code besides the sleep function, place it in
-     * setup() for example.
-     *
-     * In the function call attachInterrupt(A, B, C)
-     * A   can be either 0 or 1 for interrupts on pin 2 or 3.  
-     *
-     * B   Name of a function you want to execute at interrupt for A.
-     *
-     * C   Trigger mode of the interrupt pin. can be:
-     *             LOW        a low level triggers
-     *             CHANGE     a change in level triggers
-     *             RISING     a rising edge of a level triggers
-     *             FALLING    a falling edge of a level triggers
-     *
-     * In all but the IDLE sleep modes only LOW can be used.
-     */
- 
-    Serial.println("Sleep now");
-    sleep_mode();            // here the device is actually put to sleep!!
-                             // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
- 
-    sleep_disable();         // first thing after waking from sleep:
-                             // disable sleep...
-    //detachInterrupt(0);      // disables interrupt 0 on pin 2 so the
+  Serial.println("Sleep now");
+  sleep_mode();            // here the device is actually put to sleep!!
+  
+  // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
+  sleep_disable();         // first thing after waking from sleep: disable sleep...
+  //detachInterrupt(0);      // disables interrupt 0 on pin 2 so the
                              // wakeUpNow code will not be executed
                              // during normal running time.
   Serial.println("Out of sleep"); 
@@ -160,7 +77,7 @@ void loop()
   Serial.print(count);
   Serial.println("sec");
   count++;
-  delay(1000);                           // waits for a second
+  delay(1000);               // waits for a second
  
   // compute the serial input
   if (Serial.available()) {
@@ -188,3 +105,22 @@ void loop()
 }
  
  
+/* SLEEP MODE : In the Atmega8 datasheet
+     * http://www.atmel.com/dyn/resources/prod_documents/doc2486.pdf on page 35
+     * there is a list of sleep modes which explains which clocks and
+     * wake up sources are available in which sleep mode.
+     *
+     * In the avr/sleep.h file, the call names of these sleep modes are to be found:
+     *
+     * The 5 different modes are:
+     *     SLEEP_MODE_IDLE         -the least power savings
+     *     SLEEP_MODE_ADC
+     *     SLEEP_MODE_PWR_SAVE
+     *     SLEEP_MODE_STANDBY
+     *     SLEEP_MODE_PWR_DOWN     -the most power savings
+     *
+     * For now, we want as much power savings as possible, so we
+     * choose the according
+     * sleep mode: SLEEP_MODE_PWR_DOWN
+     *
+     */  
