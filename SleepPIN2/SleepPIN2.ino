6dbe321a3cf1e6b7826559e5cf2ffbcd3112c2a7
Audrey Marullaz <audrey.marullaz@epfl.ch>
2016-04-21 09:05:40 +0200

    Mise en forme

--- SleepPIN2/SleepPIN2.ino
+++ SleepPIN2/SleepPIN2.ino
@@ -1,107 +1,57 @@
+//////////////////////////////////////
+/// Works on the ATmega328         ///
+//////////////////////////////////////
+
 #include <avr/sleep.h>
 #include <avr/power.h>
-
 
 int pin2 = 2;
 int i;
 #define LEDR 7
 
-/***************************************************
- *  Name:        pin2Interrupt
- *
- *  Returns:     Nothing.
- *
- *  Parameters:  None.
- *
- *  Description: Service routine for pin2 interrupt  
- *
- ***************************************************/
 void pin2Interrupt(void)
 {
-  /* This will bring us back from sleep. */
-  
-  /* We detach the interrupt to stop it from 
-   * continuously firing while the interrupt pin
-   * is low.
-   */
+  /* Detach interrupt to stop it from continuously firing while pin is low. */
   detachInterrupt(0);
 }
 
-
-/***************************************************
- *  Name:        enterSleep
- *
- *  Returns:     Nothing.
- *
- *  Parameters:  None.
- *
- *  Description: Enters the arduino into sleep mode.
- *
- ***************************************************/
 void enterSleep(void)
 {
-  
   /* Setup pin2 as an interrupt and attach handler. */
   attachInterrupt(0, pin2Interrupt, LOW);
   delay(100);
   
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  
   sleep_enable();
-  
   sleep_mode();
-  
   /* The program will continue from here. */
   
   /* First thing to do is disable sleep. */
   sleep_disable(); 
 }
 
-
-/***************************************************
- *  Name:        setup
- *
- *  Returns:     Nothing.
- *
- *  Parameters:  None.
- *
- *  Description: Setup for the Arduino.           
- *
- ***************************************************/
 void setup()
 {
   Serial.begin(9600);
   
-  /* Setup the pin direction. */
   pinMode(pin2, INPUT);
   pinMode(LEDR, OUTPUT);
   //delay(8000);
   //Serial.println("Initialisation complete.");
 }
 
-
-
-/***************************************************
- *  Name:        loop
- *
- *  Returns:     Nothing.
- *
- *  Parameters:  None.
- *
- *  Description: Main application loop.
- *
- ***************************************************/
-int seconds=0;
+int seconds = 0;
 void loop()
 {
-  delay(2000);
+  //delay(2000);
   seconds++;
 
   for(i=0; i<seconds;i++)
-  {digitalWrite(LEDR, HIGH);
-  delay(500);
-  digitalWrite(LEDR, LOW);
-  delay(500);}
+  { digitalWrite(LEDR, HIGH);
+    delay(500);
+    digitalWrite(LEDR, LOW);
+    delay(500);
+  }
   
   if(seconds == 3)
   {
